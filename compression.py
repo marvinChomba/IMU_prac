8f8384727902a4bdc48d302ef815afef692eead9
marvinChomba <marvin.chomba24@gmail.com>
2021-10-03 00:35:12 +0200

    file

--- compression.py
+++ compression.py
@@ -1,9 +1,20 @@
 import zlib
-import binascii
 
-data = "Hello world"
 
-compressed_data = zlib.compress(data,2)
+original_data = open('2018-09-19-04_22_21_VN100.csv','rb').read()
+compressed_data = zlib.compress(original_data,zlib.Z_BEST_COMPRESSION)
 
-print("Original data: " + data)
-print("Compressed data: " + binascii.hexlify(compressed_data))
+#compress_ratio = (float(len(original_data)) - float(len(compressed_data))) / float(len(original_data))
+
+
+compress_ratio = (float(len(original_data)) - float(len(compressed_data))) / float(len(original_data))
+
+print('Compressed: %d%%' % (100.0 * compress_ratio))
+
+
+f = open('outfile.txt','wb')
+f.write(compressed_data)
+f.close()
+
+
+
