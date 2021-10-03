5be3bc8d60322b7300608cd37f4f9088a0444138
marvinChomba <marvin.chomba24@gmail.com>
2021-10-03 23:54:22 +0200

    hh

--- test.py
+++ test.py
@@ -1,23 +1,24 @@
 import os
+import sys
  
-file_size = os.path.getsize('./g6.csv')
-print("File Size is :", file_size, "bytes")
+file_size = os.path.getsize(sys.argv[2])
+print("File Size of " + sys.argv[2] + " is :", file_size, "bytes")
 
 #2018-09-19-03_57_11_VN100.csv
 
-file_size2 = os.path.getsize('./cities.csv')
-print("Main File Size is :", file_size2, "bytes")
+file_size2 = os.path.getsize(sys.argv[1])
+print("Main File Size(" + sys.argv[1] + "  is :", file_size2, "bytes")
 
 
 if(file_size2 > file_size):
     print("Yessirski compress")
     
     
-file_size3 = os.path.getsize('./g8.csv')
-print("File Size is :", file_size3, "bytes")
+#file_size3 = os.path.getsize('./g8.csv')
+#print("File Size is :", file_size3, "bytes")
 
 
-if(file_size3 == file_size):
-   print("Yessirski lossless compression")
-elif(file_size3 < file_size2):
-   print("Lossy compression.Smaller by {} bytes".format(file_size2 - file_size3))
+#if(file_size3 == file_size):
+ #  print("Yessirski lossless compression")
+#elif(file_size3 < file_size2):
+ #  print("Lossy compression.Smaller by {} bytes".format(file_size2 - file_size3))
