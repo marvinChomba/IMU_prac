3693aa0ad7f60e2b9a88b1880917f5f4d836dec6
marvinChomba <marvin.chomba24@gmail.com>
2021-10-03 23:41:10 +0200

    y

--- compression.py
+++ compression.py
@@ -2,7 +2,7 @@ import zlib
 
 def compress_file(file_name1,file_name2):
 
-    original_data = open(file_name1,'rb').read()
+    original_data = open(sys.argv[0],'rb').read()
     compressed_data = zlib.compress(original_data,zlib.Z_BEST_COMPRESSION)
 
     #compress_ratio = (float(len(original_data)) - float(len(compressed_data))) / float(len(original_data))
@@ -13,7 +13,7 @@ def compress_file(file_name1,file_name2):
     print('Compressed: %d%%' % (100.0 * compress_ratio))
 
 
-    f = open(file_name2,'wb')
+    f = open(sys.argv[0],'wb')
     f.write(compressed_data)
     f.close()
 
--- plot.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import plotly.express as px
-
-df = pd.read_csv('2018-09-19-03_57_11_VN100.csv')
-
-fig = px.line(df, x = 'AAPL_x', y = 'AAPL_y', title='Apple Share Prices over time (2014)')
-fig.show()
